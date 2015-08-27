bbc89d2acca80d413839dbd469f6889b9624a945
jjohnson <jonathanjohnson@iheartmedia.com>
2015-08-27 16:49:31 -0400

    Removed unused files

--- debuggingCog.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEBUGGING_COG_H
-#define DEBUGGING_COG_H
-void debugPrint(char* string , int num, ...);
-void readTouchButtons(unsigned int * states);
-void handleInputs(unsigned int * states);
-void debuggingCogMain();
-#endif
